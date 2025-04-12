865793c8518e66a7dd44bd28fc1a545840e1d412
Denis Krivolapov <qartoonnetwork@gmail.com>
2025-04-12 19:25:46 +0500

    remove useless

--- src/main/core/utils/Logger.ts
+++ src/main/core/utils/Logger.ts
@@ -1,8 +1,6 @@
 import log from "electron-log/main";
 import { FormatParams }  from "electron-log"
 
-
-
 export function settingLog() {
     log.transports.console.format = `[{h}:{i}:{s}] [{level}] {label}: {text}`;
     log.transports.console.level = "debug";
--- src/types/IPCChannels.ts
+++ src/types/IPCChannels.ts
@@ -2,10 +2,4 @@ export enum IPCChannels {
     DIALOG_OPEN_FILE = "dialog_open_file",
     OPEN_FILE = "open_file",
     WORKER_DIR = "workerDir",
-    LOG_ERROR = "log:error",
-    LOG_WARN = "log:wanr",
-    LOG_INFO = "log:info",
-    LOG_VERBOSE = "log:verbose",
-    LOG_DEBUG = "log:debug",
-    LOG_SILLY = "log:silly"
 }
