fbda653e38533e43b9af83c801e6b63fd13552ce
Denis Krivolapov <qartoonnetwork@gmail.com>
2025-04-23 21:01:41 +0500

    add config for quasar

--- src/render/main.ts
+++ src/render/main.ts
@@ -1,25 +1,15 @@
 import App from "./App.vue"
 import { createApp } from "vue"
 import log from "electron-log/renderer"
-import quasarLang from 'quasar/lang/ru'
 import { Quasar } from "quasar"
-import * as quasarComponents from 'quasar';
+
 import "@/render/core/style/quasar.variables.sass"
 import "@/render/core/style/quasar.style.sass"
 import "@quasar/extras/material-icons/material-icons.css"
+import { quasarConfig } from "./quasar.config"
+
 log.variables.label = "render";
 
-
-
-
 const app = createApp(App);
-app.use(Quasar, {
-    lang: quasarLang,
-    components: quasarComponents,
-    plugins: {},
-    config: {},
-    framework: {
-        IconSet: "material-icons",
-    }
-})
+app.use(Quasar, quasarConfig)
 app.mount("#q-app");
--- src/render/reader.ts
+++ src/render/reader.ts
@@ -1,22 +1,15 @@
 import { createApp } from "vue"
 import log from "electron-log/renderer"
-import quasarLang from 'quasar/lang/ru'
 import { Quasar } from "quasar"
-import * as quasarComponents from 'quasar';
+import { quasarConfig } from "./quasar.config"
+import ReaderView from "./core/views/ReaderView.vue"
+
 import "@/render/core/style/quasar.variables.sass"
 import "@/render/core/style/quasar.style.sass"
 import "@quasar/extras/material-icons/material-icons.css"
-import ReaderView from "./core/views/ReaderView.vue"
+
 log.variables.label = "render";
 
 const reader = createApp(ReaderView);
-reader.use(Quasar, {
-    lang: quasarLang,
-    components: quasarComponents,
-    plugins: {},
-    config: {},
-    framework: {
-        IconSet: "material-icons",
-    }
-});
+reader.use(Quasar, quasarConfig);
 reader.mount("#q-reader");
