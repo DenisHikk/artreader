a5c7f1ed39b5a2f205de1e7def918581ab90d14c
Rui Blizzgork <ruiblizzgork@gmail.com>
2025-05-11 17:43:08 +0500

    Improving readability TabsComponent.vue

--- src/render/App.vue
+++ src/render/App.vue
@@ -1,56 +1,56 @@
 <template>
-  <q-layout view="lHh lpR fFf">
-    <q-header class="bg-primary text-white">
-      <!--  Upper toolbar-->
-      <q-toolbar>
-        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
-        <!-- Title -->
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
-          ArtReader
-        </q-toolbar-title>
-      </q-toolbar>
-    </q-header>
-    
-    <!-- Left Drawer -->
-    <q-drawer v-model="leftDrawerOpen" side="left" bordered>
-      <div>
-        Pages
-      </div>
-    </q-drawer>
+    <q-layout view="lHh lpR fFf">
+        <q-header class="bg-primary text-white">
+            <!--  Upper toolbar-->
+            <q-toolbar>
+                <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+                <!-- Title -->
+                <q-toolbar-title>
+                    <q-avatar>
+                        <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+                    </q-avatar>
+                    ArtReader
+                </q-toolbar-title>
+            </q-toolbar>
+        </q-header>
 
-    <!-- Page -->
-    <q-page-container>
-      
-      <!-- Tabs -->
-      <TabsComponent />
+        <!-- Left Drawer -->
+        <q-drawer v-model="leftDrawerOpen" side="left" bordered>
+            <div>
+                Pages
+            </div>
+        </q-drawer>
+
+        <!-- Page -->
+        <q-page-container>
+
+            <!-- Tabs -->
+            <TabsComponent />
 
 
-    </q-page-container>
-    
-    <!-- Footer -->
-    <q-footer elevated class="bg-grey-8 text-white">
-      <q-toolbar>
-        <q-toolbar-title>
-          <div>
-            Pu-pu-puuuu... 
-            I hope there will be something here, for example, tools.
-          </div>
-        </q-toolbar-title>
-      </q-toolbar>
-    </q-footer>
-  </q-layout>
-</template> 
+        </q-page-container>
+
+        <!-- Footer -->
+        <q-footer elevated class="bg-grey-8 text-white">
+            <q-toolbar>
+                <q-toolbar-title>
+                    <div>
+                        Pu-pu-puuuu...
+                        I hope there will be something here, for example, tools.
+                    </div>
+                </q-toolbar-title>
+            </q-toolbar>
+        </q-footer>
+    </q-layout>
+</template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import TabsComponent from "./vue_components/tabs/TabsComponent.vue"
+    import { ref } from 'vue'
+    import TabsComponent from "./vue_components/tabs/TabsComponent.vue"
 
-const leftDrawerOpen = ref(false);
-const toggleLeftDrawer = () => {
-    leftDrawerOpen.value = !leftDrawerOpen.value
-}
+    const leftDrawerOpen = ref(false);
+    const toggleLeftDrawer = () => {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+    }
 
 </script>
--- src/render/vue_components/tabs/TabsComponent.vue
+++ src/render/vue_components/tabs/TabsComponent.vue
@@ -1,82 +1,65 @@
+<!-- Tabs -->
 <template>
-  <div>
-    <!-- Tabs -->
-    <q-tabs
-      class="bg-secondary text-white"
-      v-model="activeTab"
-      :key="tabs.length"
-      align="left"
-    >
-      <q-tab
-        v-for="(tab, index) in tabs" :key="tab.id" :name="tab.id"
-        @dragstart="onDragStart($event, index)"
-        @dragover.prevent
-        @drop="onDrop($event, index)"
-        draggable="true"
-      >
-        <div>
-          <span>
-            {{ tab.name }} {{ tab.id }}
-          </span>
-          <q-btn 
-            @click.stop="deleteTab(tab.id)"
-            @mousedown.stop
-            dense
-            size="sm"
-            icon="delete" 
-            color="primary" 
-          />
-        </div>
-      </q-tab>
-      <q-btn @click="addTab()" round label="+" color="primary" />
-    </q-tabs>
-  </div>
-  <div 
-    class="q-mt-md"
-  >
-    <div 
-      v-if="tabs.length !== 0"
-    >
-      <div>
-        Содержимое: {{ activeTab }}; Количество вкладок: {{ tabs.length }};
-        Индекс текущей вкладки: 
-          {{ tabs.findIndex(tab => tab.id === activeTab) }}
-        Файл текущей вкладки:
-          {{ tabs[tabs.findIndex(tab => tab.id === activeTab)].filepath }}
-      </div>
-      
-      <div 
-        v-if="tabs[tabs.findIndex(tab => tab.id == activeTab)].filepath == 'none'"
-      >
-        <q-btn label="Open File" @click="openFile"/>
-      </div>
-      <div 
-        v-if="tabs[tabs.findIndex(tab => tab.id == activeTab)].filepath !== 'none'"
-      >
-        <div>
-          <ReaderView 
-            :key="activeTab"
-            :filePath="tabs[tabs.findIndex(tab => tab.id == activeTab)].filepath" />
-        </div>
-      </div>
-      
+    <div>
+        <q-tabs 
+            class="bg-secondary text-white" 
+            v-model="activeTab" 
+            :key="tabs.length" 
+            align="left">
+            <q-tab 
+                v-for="(tab, index) in tabs" 
+                :key="tab.id" 
+                :name="tab.id" 
+                @dragstart="onDragStart($event, index)"
+                @dragover.prevent 
+                @drop="onDrop($event, index)" 
+                draggable="true">
+                    <span> {{ tab.name }} {{ tab.id }} </span>
+                    <q-btn 
+                        @click.stop="deleteTab(tab.id)" 
+                        @mousedown.stop 
+                        dense 
+                        size="sm" 
+                        icon="delete"
+                        color="primary" 
+                    />
+            </q-tab>
+            <q-btn 
+                @click="addTab()" 
+                round 
+                label="+" 
+                color="primary"
+            />
+        </q-tabs>
     </div>
-  </div>
+    <div class="q-mt-md">
+        <div v-if="tabs.length !== 0">
+            <div v-if="tabs[tabs.findIndex(tab => tab.id == activeTab)].filepath == 'none'">
+                <q-btn label="Open File" @click="openFile" />
+            </div>
+            <div v-else>
+                <ReaderView 
+                    :key="activeTab"
+                    :filePath="tabs[tabs.findIndex(tab => tab.id == activeTab)].filepath" 
+                />
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
-import useTabs from './tabs';
-import ReaderView from '@/render/core/views/ReaderView.vue';
-const {
-    leftDrawerOpen,
-    tabs,
-    activeTab,
+    import useTabs from './tabs';
+    import ReaderView from '@/render/core/views/ReaderView.vue';
+    const {
+        leftDrawerOpen,
+        tabs,
+        activeTab,
 
-    toggleLeftDrawer,
-    addTab,
-    deleteTab,
-    openFile,
-    onDragStart,
-    onDrop
-} = useTabs();
+        toggleLeftDrawer,
+        addTab,
+        deleteTab,
+        openFile,
+        onDragStart,
+        onDrop
+    } = useTabs();
 </script>
